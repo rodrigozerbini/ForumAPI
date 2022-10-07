7685fce2aaed557331a0bb7bb92b8e83fd77c77b
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2022-10-07 16:20:57 +0200

    Add @CrossOrigin annotation to controllers

--- forum/src/main/java/com/solera/forum/controllers/UserController.java
+++ forum/src/main/java/com/solera/forum/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.solera.forum.controllers;
 import com.solera.forum.models.User;
 import com.solera.forum.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
